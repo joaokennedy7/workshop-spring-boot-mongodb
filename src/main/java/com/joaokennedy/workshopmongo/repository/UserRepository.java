c54db68eaf105f663a1a72c9a08823899445f9ca
Joao Kennedy <joao.kennedy707@gmail.com>
2020-09-25 04:11:24 -0300

    Your branch is up to date with 'origin/master'.

--- src/main/java/com/joaokennedy/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/joaokennedy/workshopmongo/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.joaokennedy.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.joaokennedy.workshopmongo.resource.User;
+import com.joaokennedy.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
